43651fddff206d20ab1838ff5bb5f55b8d0cbc13
= <DouglassHooker@gmail.com>
2018-04-02 13:34:02 -0500

    Initial commit

---
+++ styles/colors.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export const colors = {
+  primary: '#5755d9',
+  secondary: '#f1f1fc',
+  gray: '#454d5d',
+  light: '#f8f9fa',
+  success: '#32b643',
+  warning: '#ffb700',
+  error: '#e85600'
+}
